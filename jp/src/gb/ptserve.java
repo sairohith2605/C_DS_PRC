017997534ab985c2478b4d5cabb67f636e17b698
vamshi264 <59801989+vamshi264@users.noreply.github.com>
2020-11-02 20:40:11 +0530

    Add files via upload

---
+++ jp/src/gb/ptserve.java
@@ -0,0 +1,19 @@
+package gb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ptserve extends HttpServlet
+{
+	public void service(HttpServletRequest req , HttpServletResponse res) throws IOException
+	{
+		int i = Integer.parseInt(req.getParameter("num1"));
+		PrintWriter out = res.getWriter();
+		
+		out.println(i);
+	}
+}
